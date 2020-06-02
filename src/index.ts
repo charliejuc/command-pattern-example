32f06833ca842d166215ca45d64a577f35427a9f
charliejuc <charliejuc@gmail.com>
2020-06-02 13:34:36 +0200

    Final command pattern

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,29 @@ import 'module-alias/register'
 import { MultiplyCommand } from './implementations/MultiplyCommand'
 import { ICommand } from './interfaces/ICommand'
 
-const multiplyCommand = new MultiplyCommand(5)
+const multiplier = 5
 let value = 10
 const commands: ICommand[] = []
 
 console.log('BEFORE', value)
 
+let multiplyCommand = new MultiplyCommand(multiplier)
 value = multiplyCommand.execute(value)
-commands.push(multiplyCommand)
+commands.unshift(multiplyCommand)
+
+multiplyCommand = new MultiplyCommand(multiplier)
+commands.unshift(multiplyCommand)
+
+multiplyCommand = new MultiplyCommand(multiplier)
+commands.unshift(multiplyCommand)
 
 console.log('EXECUTED', value)
 
 // value = multiplyCommand.undo(value)
 for (const command of commands) {
     value = command.undo()
+
+    console.log('UNDONE LOOP', value)
 }
 
 console.log('UNDONE', value)
